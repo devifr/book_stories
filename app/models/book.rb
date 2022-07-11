0119154a1b4eaa256114f101fa1d6061ccf1b7ea
Devi <dfedogawa3@gmail.com>
2022-07-12 06:54:37 +0700

    update search

--- app/models/book.rb
+++ app/models/book.rb
@@ -11,8 +11,8 @@ class Book < ApplicationRecord
     search = self
     search = search.where(author_id: params[:author_id]) if params[:author_id].present?
     search = search.where(year: params[:year]) if params[:year].present?
-    search = search.where("page >= ?",params[:from_page]) if params[:from_page].present?
-    search = search.where("page <= ?",params[:to_page]) if params[:to_page].present?
+    search = search.where("pages >= ?",params[:from_page]) if params[:from_page].present?
+    search = search.where("pages <= ?",params[:to_page]) if params[:to_page].present?
     search
   end
 end
